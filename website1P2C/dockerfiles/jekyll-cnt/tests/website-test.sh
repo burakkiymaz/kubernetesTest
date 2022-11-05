836867d188c170b2635ea280472bf976bd7c467b
Burak Kıymaz <kiymazburak58@gmail.com>
2022-11-05 19:55:06 +0300

    test case improved

--- website1P2C/dockerfiles/jekyll-cnt/tests/website-test.sh
+++ website1P2C/dockerfiles/jekyll-cnt/tests/website-test.sh
@@ -1,11 +1,20 @@
 contentTest=`curl -s localhost:4000 | head -n1 | cut -d ">" -f 2 | cut -d "<" -f 1`
 responseTest=`curl -s -o /dev/null -w "%{http_code}" localhost:4000`
 
-if [[ $responseTest = 200 && $contentTest = "TEST" ]];
+if [[ $responseTest = 200 ]];
 then
-    echo success
+    echo Response test successful
     exit 0
 else 
-    echo failed
+    echo Test failed because response expected 200 but returned $responseTest 
+    exit 1
+fi
+
+if [[ $contentTest = "TEST" ]];
+then
+    echo Content test successful
+    exit 0
+else 
+    echo Test failed because response expected TEST but returned $contentTest
     exit 1
 fi
