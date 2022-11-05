88de6f54679517893aefca7248ef92c7d6c28c96
Burak Kıymaz <kiymazburak58@gmail.com>
2022-11-05 16:10:49 +0300

    app test

--- website1P2C/dockerfiles/jekyll-cnt/jekyll.groovy
+++ website1P2C/dockerfiles/jekyll-cnt/jekyll.groovy
@@ -5,11 +5,17 @@ def buildApp() {
     jekyllImage = docker.build("burakkiymaz/website-build:alpha", "-f website1P2C/dockerfiles/jekyll-cnt/Dockerfile website1P2C/dockerfiles/jekyll-cnt")
 }
 
+def testApp() {
+    echo "Jekyll image testing"
+    sh 'docker run -d -n website-build -p 4000:4000 burakkiymaz/website-build:alpha'
+    sh 'docker exec -it website-build bash'
+    sh 'curl localhost:4000;exit'
+    sh 'docker stop website-build; docker rm website-build'
+}
+
 def deployApp() {
     echo "Jekyll application deploying"
-    docker.withRegistry('', registryCredential) {
-        jekyllImage.push('alpha')
-    }
+    sh 
 }
 
 return this
