4b99ddb3e1113a4d6f66255ee55ea956878971af
Burak Kıymaz <kiymazburak58@gmail.com>
2022-11-05 19:05:18 +0300

    application test script added

---
+++ website1P2C/dockerfiles/jekyll-cnt/tests/website-test.sh
@@ -0,0 +1,9 @@
+contentTest=`curl -s localhost:4000 | head -n1 | cut -d ">" -f 2 | cut -d "<" -f 1`
+responseTest=`curl -s -o /dev/null -w "%{http_code}" localhost:4000`
+
+if [[ $responseTest = 200 && $contentTest = "TEST" ]];
+then
+    echo success
+else 
+    echo failed
+fi
